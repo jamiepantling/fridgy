268e649af469cbb5afcc76f27d4e05cb5c0903a8
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-05-22 12:05:01 -0400

    shared and off-limits foods

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path("foods/", views.foods_index, name="index"),
     # accounts url patterns
     path('accounts/signup/', views.signup, name='signup'),
 ]

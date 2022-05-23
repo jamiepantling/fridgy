b1ba982366e1bf2f1c5335c8a13088dabfa79c1e
Thomas Lawlor <thomaslawlor@Thomass-MacBook-Pro.local>
2022-05-23 12:55:35 -0400

    Finished household CRUD backend

--- app/views.py
+++ app/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import redirect, render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 
 # import login
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
-from .models import Food
+from .models import Food, Household, Profile
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
+# Food functions
 
 def foods_index(request):
     foods = Food.objects.all
     return render(request, 'foods/index.html', {"foods": foods})
+
+# Household functions
+
+def household_index(request):
+    household = Household.objects.all()
+    return render(request, 'households/index.html', {'household': household})
+def household_detail(request, household_id):
+    household = Household.objects.get(id=household_id)
+    users_in_house = Profile.objects.filter(household=household_id)
+    return render(request, 'households/detail.html', { 'household': household, 'users_in_house': users_in_house })
+
+# Household class-based views
+
+class HouseholdCreate(CreateView):
+    model = Household
+    fields = ['name']
+    success_url = '/' # Changed when new route finished
+class HouseholdUpdate(UpdateView):
+    model = Household
+    fields = ['name']
+    success_url = '/' # Changed later to household details
+class HouseholdDelete(DeleteView):
+    model = Household
+    success_url = '/' # Change success?
+
 
 # Authorization functions
 def signup(request):

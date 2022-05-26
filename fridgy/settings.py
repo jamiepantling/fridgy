4ccab7eb3dc9d5597cc41d42674b3716c13bb53a
Thomas Lawlor <thomaslawlor@Thomass-MacBook-Pro.local>
2022-05-26 12:43:34 -0400

    Config deployment

--- fridgy/settings.py
+++ fridgy/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.env['SECRET_KEY']
-SECRET_KEY='django-insecure-ymtw(jxukvk1n+nggkrrn3@*l$a(lt+c@i5py%!$b(+^f)$8xq'
+SECRET_KEY = os.env['SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
+    'theme.apps.ThemeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
